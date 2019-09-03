a94cc74a2151627967b341b32ad511c2c9b1d540
Thiago Amendola <amendolathiago@gmail.com>
2019-09-03 19:25:02 -0300

    Remove base64 IAP receipt broken conversion for now

--- platform/iphone/in_app_store.mm
+++ platform/iphone/in_app_store.mm
@@ -222,7 +222,12 @@ Error InAppStore::restore_purchases() {
 					receipt_to_send = [receipt description];
 				}
 				Dictionary receipt_ret;
-				receipt_ret["receipt"] = [(receipt_to_send != nil ? [receipt_to_send base64EncodedStringWithOptions:0] : @"") UTF8String];
+				if (receipt_to_send != nil){
+					// receipt_ret["receipt"] = [[receipt_to_send base64EncodedStringWithOptions:0] UTF8String];
+					receipt_ret["receipt"] = "";
+				}else{
+					receipt_ret["receipt"] = "";
+				}
 				receipt_ret["sdk"] = sdk_version;
 				ret["receipt"] = receipt_ret;
 
