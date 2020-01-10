cd91cd0219db047b89e16d34cb3e501a9eecc765
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-01-10 16:21:04 -0300

    Remove calls to enable/disable rendering on focus in and focus out
    We're now doing it through GDscript using virtual server

--- platform/iphone/app_delegate.mm
+++ platform/iphone/app_delegate.mm
@@ -155,7 +155,6 @@ void _ios_add_joystick(GCController *controller, AppDelegate *delegate) {
 static void on_focus_out(ViewController *view_controller, bool *is_focus_out) {
 	if (!*is_focus_out) {
 		*is_focus_out = true;
-		Main::disable_render();
 		if (OS::get_singleton()->get_main_loop())
 			OS::get_singleton()->get_main_loop()->notification(
 					MainLoop::NOTIFICATION_WM_FOCUS_OUT);
@@ -174,7 +173,6 @@ static void on_focus_out(ViewController *view_controller, bool *is_focus_out) {
 static void on_focus_in(ViewController *view_controller, bool *is_focus_out) {
 	if (*is_focus_out) {
 		*is_focus_out = false;
-		Main::enable_render();
 		if (OS::get_singleton()->get_main_loop())
 			OS::get_singleton()->get_main_loop()->notification(
 					MainLoop::NOTIFICATION_WM_FOCUS_IN);
